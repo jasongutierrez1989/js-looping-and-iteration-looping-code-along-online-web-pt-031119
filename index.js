9a3d67f7ae5cb4db99b9f1fbec4c2d1c9ef8c773
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-09-26 23:49:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function writeCards(names, event_name) {
   for (let i = 0; i < names.length; i++) {
     console.log('Thank you, ${names[i]}, for the wonderful ${event_name} gift!');
   }
-  return
 }
 
 function countdown(num) {
